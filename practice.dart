0c969d72d86d963f9798f5c55744c573b2f8593e
Xander Fleming <Xander.Fleming@IST-Sidney1.local>
2026-05-12 14:13:24 -0400

    finished student practice

--- practice.dart
+++ practice.dart
@@ -1,17 +1,17 @@
 import 'dart:io';
 
 void main() {
-  var studentList = [];
-  String ? studentName, favoriteSubject;
+  List<String> studentClasses = [];
+  String ? studentName, favoriteSubject,studentClass;
   int ? studentAge, studentGrade;
   bool goodSubject = false;
   print("Enter your name:");
   studentName = stdin.readLineSync();
   print("Enter your age:");
-  studentAge = stdin.readByteSync();
+  studentAge = int.parse(stdin.readLineSync()!);
   do {
     print("Enter your grade level:");
-    studentGrade = stdin.readByteSync();
+    studentGrade = int.parse(stdin.readLineSync()!);
     if (studentGrade < 1 || studentGrade > 12)
     {
       print("Please enter a valid grade level.\n");
@@ -31,4 +31,17 @@ void main() {
       goodSubject = false;
     }
   } while (!goodSubject);
+
+  for(int i = 1; i <= 4; i++)
+  {
+    print("Enter class number: $i");
+    studentClass = stdin.readLineSync();
+    studentClasses.add(studentClass!);
+  }
+
+  showProfile(studentName, favoriteSubject, studentClasses, studentAge, studentGrade);
+}
+void showProfile(String? name, String? favoriteSubject, List<String> studentClasses, int studentAge, int studentGrade)
+{
+  print("$name is a $studentAge year old $studentGrade grader whos favorite subject is $favoriteSubject and he takes ${studentClasses[0]},${studentClasses[1]},${studentClasses[2]},${studentClasses[3]}");
 }
