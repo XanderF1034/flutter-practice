431858e2fdc2a1cc6b7b053a12e2d9b138d70621
Xander Fleming <xfleming22713@gmail.com>
2026-05-16 19:02:44 -0400

    finished this student profile page thing, gonna move onto developing the real app

--- multi_page_app/lib/pages/ProfilePage.dart
+++ multi_page_app/lib/pages/ProfilePage.dart
@@ -12,7 +12,7 @@ class Profilepage extends StatefulWidget {
 class _Profilepagestate extends State<Profilepage> {
   final Student xander = Student("Xander", "Fleming", "Science", 11, 17, 4.833);
   final Student ty = Student("Ty", "Allen", "Trigonometry", 11, 17, 2.7);
-  final Student zack = Student("Zachary", "Siegel", "ELA", 11, 17, 4.5);
+  final Student zack = Student("Zachary", "Siegel", "ELA", 11, 17, 3.49);
   final searchEditor = TextEditingController();
 
   @override
@@ -46,12 +46,12 @@ class _Profilepagestate extends State<Profilepage> {
               if (filteredStudents.isNotEmpty) ...[
                 SizedBox(height: 75),
                 Expanded(
-                child: ListView.builder(
-                  itemCount: filteredStudents.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 20),
-                      child: ElevatedButton(
+                  child: ListView.builder(
+                    itemCount: filteredStudents.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        /*child: ElevatedButton(
                         onPressed: () {
                           Navigator.push(
                             context,
@@ -70,25 +70,88 @@ class _Profilepagestate extends State<Profilepage> {
                             fontSize: 30,
                           ),
                         ),
-                      ),
-                    );
-                  },
+                      ),*/
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => Studentinfopage(
+                                  student: filteredStudents[index],
+                                ),
+                              ),
+                            );
+                          },
+                          child: Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: Row(
+                                children: [
+                                  CircleAvatar(
+                                    radius: 80,
+                                    backgroundColor: Colors.black,
+                                    child: Text(
+                                      filteredStudents[index].firstName[0],
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 50,
+                                      ),
+                                      ),
+                                  ),
+                                  SizedBox(width: 50),
+                                  Column(
+                                    children: [
+                                      Text(
+                                        '${filteredStudents[index].firstName} ${filteredStudents[index].lastName}',
+                                        style: const TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 30,
+                                        ),
+                                      ),
+                                      Text(
+                                        '${filteredStudents[index].gradeLevel}',
+                                        style: const TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 30,
+                                        ),
+                                      ),
+                                      Text(
+                                        '${filteredStudents[index].favoriteSubject}',
+                                        style: const TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 30,
+                                        ),
+                                      ),
+                                      Text(
+                                        '${filteredStudents[index].gpa}',
+                                        style: TextStyle(
+                                          color: getGpaColor(filteredStudents[index].gpa),
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 30,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 ),
-                ),
-              ]
-              else ...[
+              ] else ...[
                 SizedBox(height: 50),
                 const Text(
                   "No Students found.",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 50,
-                  )
-                  
-                )
-
-              ]
-                
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+                ),
+              ],
             ],
           ),
         ),
@@ -100,5 +163,20 @@ class _Profilepagestate extends State<Profilepage> {
   void dispose() {
     searchEditor.dispose();
     super.dispose();
+  }
+  Color getGpaColor(double gpa)
+  {
+    if (gpa >= 3.5)
+    {
+      return Colors.green;
+    }
+    else if (gpa >= 3.0 && gpa < 3.5)
+    {
+      return Colors.orange;
+    }
+    else
+    {
+      return Colors.red;
+    }
   }
 }
